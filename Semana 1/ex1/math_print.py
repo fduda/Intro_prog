10c237db3cf5e95e76e4b00b4b948c11cd78b0d7
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2020-10-22 21:49:44 +0300

    termina o codigo de math_print

--- Semana 1/ex1/math_print.py
+++ Semana 1/ex1/math_print.py
@@ -3,4 +3,12 @@ import math
 def cos_45():
     print(math.cos(45))
 
+def natural_log_20():
+    print(math.log(20))
+
+def triangle_area_3x12():
+    print(12*3/2)
+
 cos_45()
+natural_log_20()
+triangle_area_3x12()
