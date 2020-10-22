95f2fc4c11c347e2bd6da449c9e869fb8b4e3765
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2020-10-22 15:32:53 +0300

    adds hello.py and readme

---
+++ Semana 1/ex0/hello.py
@@ -0,0 +1,8 @@
+############################################################
+# FILE: hello.py
+# WRITER: Felipe Duda Wainberg, felipew, 341299881
+# EXERCISE: intro2cs ex0 2020-2021
+# DESCRIPTION: A simple program that prints "Hello World!"
+# to the standard output (screen).
+############################################################
+print("Hello World!")
