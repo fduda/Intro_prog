b77e0ff564efb2aac5917ede4b8ac50bdf1c91e9
Felipe Duda Wainberg <felipeduda1@gmail.com>
2020-10-23 13:13:37 +0300

    adds README

--- Semana 1/ex1/math_print.py
+++ Semana 1/ex1/math_print.py
@@ -2,13 +2,17 @@ import math  # Imports the math library
 
 
 def cos_45():  # Defines the cos_45 function
-    print(math.cos(45))  # Summons the cosine function, applies to it the value of 45 radians and prints the result
+    # Summons the cosine function, applies to it the value of 45 radians and prints the result
+    print(math.cos(45))
 
 
 def natural_log_20():  # Defines the natural_log_20 function
-    print(math.log(20))  # Summons the log function, applies to it the value of 20 and prints the result.
+    # Summons the log function, applies to it the value of 20 and prints the result.
     # Since we only applied one value to the log function, it returns the natural log.
+    print(math.log(20))
+
 
 
 def triangle_area_3x12():  # Defines the triangle_area_3x12 function.
-    print((12 * 3) / 2)  # Prints the result of the multiplication of 12 and 3, all divided by 2.
+    # Prints the result of the multiplication of 12 and 3, all divided by 2.
+    print((12 * 3) / 2)
