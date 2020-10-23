300a76aeaab511e18194abb8bb6331029ced21d2
Felipe Duda Wainberg <felipeduda1@gmail.com>
2020-10-23 08:49:13 +0300

    comenta parte do codigo de math_print

--- Semana 1/ex1/math_print.py
+++ Semana 1/ex1/math_print.py
@@ -1,13 +1,13 @@
-import math
+import math # Imports the math library
 
-def cos_45():
-    print(math.cos(45))
+def cos_45(): # Defines the cos_45 function
+    print(math.cos(45)) # Summons the cosine function, apply to it the value of 45 radians and prints the result
 
 def natural_log_20():
-    print(math.log(20))
+    print(math.log(20)) # 
 
 def triangle_area_3x12():
-    print(12*3/2)
+    print((12*3)/2)
 
 cos_45()
 natural_log_20()
