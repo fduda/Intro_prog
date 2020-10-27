cc9668033b3f88f1e6191bc8f2ff86a6861f7121
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2020-10-27 18:15:49 +0200

    termina compute_mean

---
+++ Semana 2/ex2/compute_mean.py
@@ -0,0 +1,30 @@
+def compute_mean(first_number, second_number, mean):
+    """
+    This function computes the mean.
+    The first two parameters are the numbers for the calculation.
+    The third parameter indicate the type of mean: arithmetic, geometric or harmonic.
+    """
+    if mean == "A": # Starts the arithmetic mean condition.
+        return (first_number+second_number)/2 # Returns the arithmetic mean.
+
+    if mean == "G": # Starts the geometric mean condidion.
+        product = first_number*second_number # Store the product of the parameters in a variable.
+
+        if product < 0: # Starts a condition on the negative product.
+            return None
+        else:
+            return (first_number*second_number)**(1/2) # Returns the geometric mean
+    
+    if mean == "H": # Starts the harmonic mean condition
+        if first_number == 0 or second_number == 0: # Starts a condition on the parameters signal.
+            return None
+        else:
+            return 2/(1/first_number+1/second_number) # Returns the harmonic mean.
+    
+    if mean != "A" or "G" or "H": # Starts a condition on wether the mean type is valid.
+        return None
+
+print(compute_mean(0,0,"H"))
+print(compute_mean(2,-3,"A"))
+print(compute_mean(-2,-3,"G"))
+print(compute_mean(2,3,"P"))
