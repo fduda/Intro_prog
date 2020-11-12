d7a8b0a4f3ca51d511805204ac93f8ccebb066d5
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2020-11-12 17:05:43 +0200

    termina ex3

--- Semana 3/ex3/ex3.py
+++ Semana 3/ex3/ex3.py
@@ -72,14 +72,47 @@ def lucky_tosses(lst):
     """""
     write your code here
     """""
+    counter_zero = 0
+    counter_one = 0
+    for i in lst:
+        if i == 0:
+            counter_zero +=1
+        elif i == 1:
+            counter_one += 1
 
-    # counter_zero = lst.count(0)
-    # counter_one = lst.count(1)
+    probability_zero = 100*counter_zero/len(lst)
+    probability_one = 100*counter_one/len(lst)
+    seq_bigger_than_five = greater_than_five(lst)
+    max_seq = max_sequence(lst)
 
-    # probability_zero = 100*counter_zero/len(lst)
-    # probability_one = 100*counter_one/len(lst)
-    # print(probability_one)
-    # print(probability_zero)
+    return [probability_one, probability_zero, seq_bigger_than_five, max_seq]
+
+def max_sequence(lst):
+    counter_maximum = 1
+    counter_current = 1
+
+    for i in range(1, len(lst)):
+        if lst[i] == lst[i-1]:
+            counter_current += 1
+        else:
+            if counter_current > counter_maximum:
+                counter_maximum = counter_current
+            counter_current = 1
+            
+    return counter_maximum
+
+def greater_than_five(lst):
+    seq_counter = 0
+    counter_current = 1
+    for i in range(1,len(lst)):
+        if lst[i] == lst[i-1]:
+            counter_current += 1
+        else:
+            if counter_current >=5 :
+                seq_counter += 1
+            counter_current = 1
+        
+    return seq_counter
 
 
 def cumulative_distribution(num_list, value_list):
--- Semana 3/ex3/lucky_tosses.py
+++ Semana 3/ex3/lucky_tosses.py
@@ -2,40 +2,6 @@ def lucky_tosses(lst):
     """""
     write your code here
     """""
-    counter_seq = 0
-    counter_seq_max = 0
-
-    for i in lst:
-        if lst[i] == lst[i+1]:
-            counter_seq += 1
-        elif lst[i] != lst[i+1]:
-            counter_seq_max = counter_seq
-
-    return counter_seq_max
-
-def find_max_rept(lst):
-    count_max = 1
-    count_curr = 1
-        
-    for i in range(1,len(lst)):
-        if lst[i] == lst[i-1]:
-            count_curr += 1
-        else:
-            if count_curr > count_max:
-                count_max = count_curr
-            count_curr = 1
-    if count_curr > count_max:
-        count_max = count_curr
-    
-    return count_max
-
-print(find_max_rept([1,1,0,0,1,1,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1]))
-
-
-def ratio(lst):
-    """
-    docstring
-    """
     counter_zero = 0
     counter_one = 0
     for i in lst:
@@ -44,8 +10,41 @@ def ratio(lst):
         elif i == 1:
             counter_one += 1
 
-        probability_zero = 100*counter_zero/len(lst)
-        probability_one = 100*counter_one/len(lst)
-    return probability_zero, probability_one
+    probability_zero = 100*counter_zero/len(lst)
+    probability_one = 100*counter_one/len(lst)
+    seq_bigger_than_five = greater_than_five(lst)
+    max_seq = max_sequence(lst)
+    return [probability_one, probability_zero, seq_bigger_than_five, max_seq]
+
 
 # [1,1,0,0,1,1,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1]
+# [1,1,0,0,1,1,1,1,0,0,0,1,1,1]
+def max_sequence(lst):
+    counter_maximum = 1
+    counter_current = 1
+
+    for i in range(1, len(lst)):
+        if lst[i] == lst[i-1]:
+            counter_current += 1
+        else:
+            if counter_current > counter_maximum:
+                counter_maximum = counter_current
+            counter_current = 1
+    return counter_maximum
+
+def greater_than_five(lst):
+    seq_counter = 0
+    counter_current = 1
+    for i in range(1,len(lst)):
+        if lst[i] == lst[i-1]:
+            counter_current += 1
+        else:
+            if counter_current >=5 :
+                seq_counter += 1
+            counter_current = 1
+        
+    return seq_counter
+
+# print(greater_than_five([1,1,0,0,1,1,1,1,0,0,0,1,1,1]))
+
+print(lucky_tosses([1,1,0,0,1,1,1,1,0,0,0,1,1,1]))
