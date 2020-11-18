e42b18bc0232d51abb6c9a9157e29d0d6a2430e3
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2020-11-18 20:15:06 +0200

    avanca item2

--- Semana 5/rascunho.py
+++ Semana 5/rascunho.py
@@ -2,24 +2,31 @@ import copy
 import random
 import time
 
-# # 2. Check if card is in deck and remove it
-# def remove_card(deck, card):
-#     deck_set = set()
-#     for card in deck:
-#         card = set(card)
-#         deck_set.add(card)
-#     return deck_set
+# 2. Check if card is in deck and remove it
+def remove_card(deck, card):
+
+    final_deck = []
+    for i in range(len(deck)):
+        deck[i] = frozenset(deck[i])
+    deck_set = set(deck)
+
+    card_set = set(card)
+    if card_set in deck_set:
+        deck_set.remove(card_set)
+    elif card_set not in deck_set:
+        print("Error! card is not in the deck")
+    
+    for card_set in deck_set:
+        card_list = list(card_set)
+        final_deck.append(card_list)
+
+    return final_deck
 
 
-# print(remove_card([["dolphin", "bomb","spider"],["eye", "bomb","fire"],["spider","fire","lock"],["bomb","lock","tree"]],None))
+print(remove_card([["dolphin", "bomb","spider"],["eye", "bomb","fire"],["spider","fire","lock"],["bomb","lock","tree"]],\
+    ["spider","lock","fire"]))
     
 # [["dolphin", "bomb","spider"],["eye", "bomb","fire"],["spider","fire","lock"],["bomb","lock","tree"]]
 
-def cards_intersect(card1, card2):
-    card1_set = set(card1)
-    card2_set = set(card2)
-    in_both = list(card1_set.intersection(card2_set))
-    return in_both
 
-print(cards_intersect(["tortoise","dog","zebra"],["zebra","tortoise","cheese"]))
 
