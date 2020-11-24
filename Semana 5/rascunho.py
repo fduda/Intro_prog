c38bb19f1c5f56d4c4017ade47cb1ec9a4ee386a
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2020-11-24 10:31:39 +0200

    ,

--- Semana 5/rascunho.py
+++ Semana 5/rascunho.py
@@ -2,30 +2,39 @@ import copy
 import random
 import time
 
-2. Check if card is in deck and remove it
-def remove_card(deck, card):
+# 2. Check if card is in deck and remove it
+# def remove_card(deck, card):
 
-    final_deck = []
-    for i in range(len(deck)):
-        deck[i] = frozenset(deck[i])
-    deck_set = set(deck)
+#     final_deck = []
+#     for i in range(len(deck)):
+#         deck[i] = frozenset(deck[i])
+#     deck_set = set(deck)
 
-    card_set = set(card)
-    if card_set in deck_set:
-        deck_set.remove(card_set)
+#     card_set = set(card)
+#     if card_set in deck_set:
+#         deck_set.remove(card_set)
         
-    elif card_set not in deck_set:
-        print("Error! card is not in the deck")
-        return False
+#     elif card_set not in deck_set:
+#         print("Error! card is not in the deck")
+#         return False
 
     
-    for card_set in deck_set:
-        card_list = list(card_set)
-        final_deck.append(card_list)
+#     for card_set in deck_set:
+#         card_list = list(card_set)
+#         final_deck.append(card_list)
     
-    return True
+#     return True
 
-print(remove_card([["dolphin", "bomb","spider"],["eye", "bomb","fire"],["spider","fire","lock"],["bomb","lock","tree"]],["spider","fire","lock",]))
+# print(remove_card([["dolphin", "bomb","spider"],["eye", "bomb","fire"],["spider","fire","lock"],["bomb","lock","tree"]],["spider","fire","lock",]))
     
 # [["dolphin", "bomb","spider"],["eye", "bomb","fire"],["spider","fire","lock"],["bomb","lock","tree"]]
+import math
 
+# def weird(n):
+#     return [x for x in range(2,round(math.sqrt(n))) if n%(x*x) == 0]
+
+# print(weird(9))
+
+lst = [x for x in range(2,round(math.sqrt(4))) if 4%(x*x)==0]
+
+print(round(math.sqrt(10)))
