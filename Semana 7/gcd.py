a0e76bdd563294ae663ce47138a0cb39c4acfc1a
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2020-12-02 18:12:33 +0200

    termina ex4

---
+++ Semana 7/gcd.py
@@ -0,0 +1,46 @@
+import time
+import math
+
+
+def gcd (x, y):
+    for i in range(1, min(x,y) + 1):
+        if x%i == 0 and y%i == 0:
+            divisor = i
+    return divisor
+
+
+def gcd_euclide(x, y):
+    while y>0:
+        x,y = y, x%y
+    return x
+
+# start = time.time()
+
+# print(gcd(272835546 ,870453489))
+# print("Time taken: ", round(time.time() - start,2), " seconds")
+
+# start = time.time()
+# print(gcd_euclide(272835546,870453489))
+# print("Time taken: ", round(time.time() - start, 2)," seconds") 
+
+
+# def f(n):
+#     sum = 0
+#     for i in range(n):
+#         for j in range(i):
+#             sum += (i+j)
+#     return sum
+
+
+# print(f(3))
+
+def f(x,y):
+    r = 1
+    for i in range(y):
+        r = r*x
+    return r
+
+
+
+for i in range(5): 
+    print(f(2,i))
