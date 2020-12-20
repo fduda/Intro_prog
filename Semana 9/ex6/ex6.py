017df41c6c20408531d5516c770014ed582b8253
Felipe Duda Wainberg <felipeduda1@gmail.com>
2020-12-20 15:58:08 +0200

    comenta código

--- Semana 9/ex6/ex6.py
+++ Semana 9/ex6/ex6.py
@@ -1,44 +1,62 @@
 def factorial(n):
     """
-    docstring
+    This function returns the factorial of a number.
     """
-    if n == 0:
+    if n == 0:  # Stopping condition.
         return 1
-    return n*factorial(n-1)
+    return n*factorial(n-1)  # Recursive step.
 
 
 def is_palindrome(lst):
-    if lst == return_reverse(lst):
+    """
+    This function recieves a list and verifies if it is a palindrome,
+    that's it, if the list backwards is equal to the original list.
+    """
+    # The next block compares the list with the list's backwards version.
+    if lst == return_reverse(lst): 
         return True
     else:
         return False
 
 def return_reverse(lst):
-    if len(lst) == 0:
+    """
+    This function reverses a list.
+    """
+    if len(lst) == 0:  # Stopping condition.
         return lst
     elif len(lst) == 1:
         return lst
     
-    return [lst[-1]] + return_reverse(lst[:-1])
+    return [lst[-1]] + return_reverse(lst[:-1])  # Recursive step.
 
 
 
 def sum_of_digits(n):
-    if n == 0:
+    """
+    This function returns the sum of all the numbers digits.
+    """
+    if n == 0:  # Stopping condition.
         return 0
-    return n%10 + sum_of_digits(n//10)
+    return n%10 + sum_of_digits(n//10)  # Recursive step.
 
 
     
 
 
 def convert_decimal_to_binary(n):
-    if n == 1:
+    """
+    This function converts a decimal number into its binary version.
+    """
+    if n == 1:  # Stopping condition.
         return 1
-    if n == 0:
+    if n == 0:  # Stopping condition.
         return 0
-    return int(str(convert_decimal_to_binary(n//2))+str(n%2))
+    return int(str(convert_decimal_to_binary(n//2))+str(n%2))  # Recursive step.
 
 
 def convert_binary(n):
+    """
+    This is an auxiliary function.
+    It prints the result of another function.
+    """
     print(convert_decimal_to_binary(n))
