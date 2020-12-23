218f66c8e7eb538f80a6ed2a36a8c8d5d15dab90
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2020-12-23 22:48:53 +0200

    salva esta merda

---
+++ Semana 9/ex6/coin_rascunho.py
@@ -0,0 +1,50 @@
+def _pick_coins(coins, p, pickers):
+
+    # winner = ""
+
+    # define picker
+    picker = pickers[p]
+
+    # choose how many coins to pick
+    if coins==1:
+        picked_coins = 1
+    else:
+        if coins%3==0:
+            # lost, no matter what
+            # print("{} lost here!".format(picker))
+            # doesn't matter how many coins you pick
+            picked_coins = 1
+        elif coins%3==1:
+            picked_coins = 4
+        else:
+            picked_coins = coins%3
+
+    print(coins)
+    print("{} picks {} coins".format(picker, picked_coins))
+    print("----------------")
+
+    # pick coins
+    coins = coins - picked_coins
+    # print("We are left with {} coins".format(coins))
+
+    if coins==0:
+        winner = picker
+        print("{} Wins!".format(winner))
+        return winner
+
+    # switch picker (switches between 0 and 1)
+    p = (p+1)%2
+    picker = pickers[p]
+
+    return _pick_coins(coins=coins, p=p, pickers=pickers)
+
+    
+# MAIN
+%time
+n = 1000
+pickers = ["bob","ana"]
+
+p = 0
+picker= pickers[p]
+
+winner = _pick_coins(coins=n, p=p, pickers = pickers)
