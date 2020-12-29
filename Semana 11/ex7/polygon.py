42c151fddff68bb8d3a3f6afaa61514c654290f7
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2020-12-29 19:03:50 +0200

    termina item 5

--- Semana 11/ex7/polygon.py
+++ Semana 11/ex7/polygon.py
@@ -42,3 +42,185 @@ class Polygon:
     def get_vertices(self):
         return [self.__x, self.__y]
 
+    def draw(self):
+        turtle.pd()
+        vertices = self.get_vertices()
+        angles = self.get__angles()
+        edges = self.get__edges()
+        for i in range(len(vertices[0])):
+            turtle.fd(100*edges[i])
+            turtle.left(180-angles[i])
+        turtle.done()
+    
+    def perimeter(self):
+        edges = self.get__edges()
+        perimeter = sum(edges)
+        return perimeter
+    
+    def is_convex(self):
+        angles = self.get__angles()
+        counter = 0
+        for angle in angles:
+            if angle <180:
+                counter += 1
+        if counter == len(angles):
+            return True
+        else:
+            return False
+        
+    def translate(self, x, y):
+        vertices = self.get_vertices()
+        new_vertices_x = []
+        new_vertices_y = []
+        for vert in vertices[0]:
+            new_vertices_x.append(vert+x)
+        for vert in vertices[1]:
+            new_vertices_y.append(vert+y)
+        
+        self.__x = new_vertices_x
+        self.__y = new_vertices_y
+
+    def list_of_points_cartesian(self):
+        vertices = self.get_vertices()
+        list_of_points = []
+        for i in range(len(vertices[0])):
+            list_of_points.append([vertices[0][i],vertices[1][i]])
+        return list_of_points
+
+    def list_of_points_polar(self):
+        list_of_points_polar = []
+        list_of_points_cartesian = self.list_of_points_cartesian()
+        for point in list_of_points_cartesian:
+            list_of_points_polar.append(convert_cartesian_to_polar(point))
+        return list_of_points_polar
+
+    def rotate(self, alpha):
+        points_polar_in_degrees = self.list_of_points_polar()
+        points_cartesian = []
+        new_vertices_x = []
+        new_vertices_y = []
+        
+
+        for point in points_polar_in_degrees:
+            point[1] += alpha
+        
+        for point in points_polar_in_degrees:
+            points_cartesian.append(convert_polar_to_cartesian(point))
+        
+        for point in points_cartesian:
+            new_vertices_x.append(point[0])
+            new_vertices_y.append(point[1])
+            
+
+        self.__x = new_vertices_x
+        self.__y = new_vertices_y
+
+
+
+
+
+        
+    
+
+
+
+
+def distance_between_points(point_1, point_2):
+    delta_x = point_1[0] - point_2[0]
+    delta_y = point_1[1] - point_2[1]
+    distance = math.sqrt(delta_x**2 + delta_y**2)
+    return distance
+
+def polar_angle(point):
+    x_coord = point[0]
+    y_coord = point[1]
+
+    if x_coord > 0 and y_coord > 0:
+        theta = math.atan(y_coord/x_coord)
+    elif x_coord < 0 and y_coord >0:
+        theta = math.pi - math.atan(-(y_coord/x_coord))
+    elif x_coord < 0 and y_coord < 0:
+        theta = math.pi + math.atan(y_coord/x_coord)
+    elif x_coord > 0 and y_coord < 0:
+        theta = 2*math.pi - math.atan(-(y_coord/x_coord))
+    
+    if y_coord == 0 and x_coord > 0:
+        theta = 0
+    elif y_coord ==0 and x_coord < 0:
+        theta = math.pi
+    elif x_coord == 0 and y_coord > 0:
+        theta = math.pi/2
+    elif x_coord == 0 and y_coord < 0:
+        theta = -(math.pi)/2 
+    elif x_coord == 0 and y_coord == 0:
+        theta = 0
+    return math.degrees(theta)  # In degrees.
+
+def convert_cartesian_to_polar(point):
+    radius = distance_between_points(point, (0,0))
+    theta =  polar_angle(point)
+    return [radius, theta]
+
+def convert_polar_to_cartesian(point):
+    radius = point[0]
+    theta = math.radians(point[1])  # In radians.
+    x_coord = radius*math.cos(theta)
+    y_coord = radius*math.sin(theta)
+    return [x_coord, y_coord]
+
+    
+
+
+
+# print("SQUARE")
+# square = Polygon([0,2,2,0],[0,0,2,2])
+# print("VERTICES==>" ,square.get_vertices())
+# print("ANGLES==>", square.get__angles())
+# print("EDGES==>", square.get__edges())
+# square.draw()
+# print("===============================")
+
+# print("Regular Pentagon")
+
+# pentagon = Polygon([0, 1, 1.31, 0.5, -0.31],[0, 0, 0.95, 1.54, 0.95])
+# print("VERTICES==>",pentagon.get_vertices())
+# print("ANGLES==>", pentagon.get__angles())
+# print("EDGES==>", pentagon.get__edges())
+# pentagon.draw()
+
+# print("===============================")
+# print("Non regular hexagon")
+
+# nrhexagon = Polygon([0, 2, 3, 2, 1, -1], [0, 0, 1, 2, 2, 1])
+
+# print("=====Hexagono original=====")
+# print("VERTICES==>",nrhexagon.get_vertices())
+# print("ANGLES==>", nrhexagon.get__angles())
+# print("EDGES==>", nrhexagon.get__edges())
+# nrhexagon.draw()
+
+
+
+
+# nrhexagon.rotate(45)
+
+# print("=====Hexagono rotacionado=====")
+
+# print("VERTICES==>",nrhexagon.get_vertices())
+# print("ANGLES==>", nrhexagon.get__angles())
+# print("EDGES==>", nrhexagon.get__edges())
+
+
+
+
+
+
+
+# test_square = Polygon([1, -1, -1, 1], [1, 1, -1 ,-1])
+
+# print(nrhexagon.list_of_points_cartesian())
+# print(nrhexagon.list_of_points_polar())
+
+
+
+# print(convert_polar_to_cartesian([3.1622776601683795, 18.43494882292201]))
