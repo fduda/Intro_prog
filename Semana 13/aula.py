96d6af4ec24b80a9c9234b883eb82f7835998939
Felipe Wainberg <felipeduda@DESKTOP-AFP3DUE.localdomain>
2021-01-17 21:43:56 +0200

    m

---
+++ Semana 13/aula.py
@@ -0,0 +1,12 @@
+# def count_to_num(n):
+#     if n == 0:
+#         return 
+#     yield from count_to_num(n-1)
+#     yield n
+
+# for i in count_to_num(0):
+#     print(i)
+
+
+f = print
+f("hello")
